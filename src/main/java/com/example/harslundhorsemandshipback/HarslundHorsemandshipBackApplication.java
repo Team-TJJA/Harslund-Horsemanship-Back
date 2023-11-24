fdcbf576a7bda136dce8f819e4210e3cb85fd0be
Jesper Erling Nielsen <jesp74m8@stud.kea.dk>
2023-11-24 11:34:30 +0100

    first commit

---
+++ src/main/java/com/example/harslundhorsemandshipback/HarslundHorsemandshipBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.harslundhorsemandshipback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarslundHorsemandshipBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarslundHorsemandshipBackApplication.class, args);
+    }
+
+}
