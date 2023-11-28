18794b0a3da6b89332c17ae6db4e682e63677e53
KEATLL <thom4a14@stud.kea.dk>
2023-11-28 12:07:45 +0100

    Practice done

---
+++ src/main/java/com/example/harslundhorsemandshipback/practice/services/PracticeService.java
@@ -0,0 +1,13 @@
+package com.example.harslundhorsemandshipback.practice.services;
+
+import com.example.harslundhorsemandshipback.practice.models.Practice;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PracticeService {
+    Optional<Practice> createPractice(Practice practice);
+    List<Practice> readPratices();
+    Optional<Practice> updatePractice(Practice practice);
+    boolean deletePractice(Practice practice);
+}
---
+++ src/main/java/com/example/harslundhorsemandshipback/practice/services/impl/PracticeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.harslundhorsemandshipback.practice.services.impl;
+
+import com.example.harslundhorsemandshipback.practice.models.Practice;
+import com.example.harslundhorsemandshipback.practice.repositories.PracticeRepository;
+import com.example.harslundhorsemandshipback.practice.services.PracticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PracticeServiceImpl implements PracticeService {
+    @Autowired
+    PracticeRepository practiceRepository;
+
+    @Override
+    public Optional<Practice> createPractice(Practice practice){
+        return Optional.of(practiceRepository.save(practice));
+    }
+
+    @Override
+    public List<Practice> readPratices(){
+        return practiceRepository.findAll();
+    }
+
+    @Override
+    public Optional<Practice> updatePractice(Practice practice){
+        Optional<Practice> practiceFound = practiceRepository.findById(practice.getId());
+
+        if(practiceFound.isPresent()){
+            return Optional.of(practiceRepository.save(practice));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deletePractice(Practice practice){
+        Optional<Practice> practiceFound = practiceRepository.findById(practice.getId());
+
+        if(practiceFound.isPresent()){
+            practiceRepository.delete(practiceFound.get());
+            return true;
+        }
+        return false;
+    }
+}
